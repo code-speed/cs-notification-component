3fe8037b36a58c6e336e8bfb2b18d3314603e2cb
PardeepDhanesar <pardeepdhanesar58@gmail.com>
2021-10-24 14:12:02 -0400

    Initial commit

--- src/main/java/net/codepseed/NotificationApplication.java
+++ src/main/java/net/codepseed/NotificationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsNotificationComponentApplication {
+public class NotificationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsNotificationComponentApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(NotificationApplication.class, args);
+   }
 
 }
