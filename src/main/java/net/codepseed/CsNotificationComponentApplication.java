bd08d6448ce4925371d6136696aadd42c9fba950
PardeepDhanesar <pardeepdhanesar58@gmail.com>
2021-10-24 14:08:10 -0400

    Initial commit

---
+++ src/main/java/net/codepseed/CsNotificationComponentApplication.java
@@ -0,0 +1,13 @@
+package net.codepseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsNotificationComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsNotificationComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/codepseed/CsNotificationComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codepseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsNotificationComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
